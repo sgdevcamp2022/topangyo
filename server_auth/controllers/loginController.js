84e69f7a97f458e4e83de6810e8d86edb4031335
smaivnn <janus321@naver.com>
2023-01-25 21:59:24 +0900

    feat : auth server v1

---
+++ server_auth/controllers/loginController.js
@@ -0,0 +1,55 @@
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const handleLogin = async (req, res) => {
+  const { id, password } = req.body;
+  if (!id || !password) return res.sendStatus(400);
+
+  if (id && typeof id !== "string") return res.sendStatus(400);
+  if (password && typeof password !== "string") return res.sendStatus(400);
+
+  const foundUser = await User.findOne({ id });
+  if (!foundUser) return res.sendStatus(204);
+
+  const match = await bcrypt.compare(password, foundUser.password);
+  if (match) {
+    // const roles = Object.values(foundUser.roles);
+    const accessToken = jwt.sign(
+      {
+        userInfo: {
+          id: foundUser.id,
+          nickname: foundUser.nickname,
+          gender: foundUser.gender,
+          roles: foundUser.roles,
+        },
+      },
+      process.env.ACCESS_TOKEN_SECRET,
+      { expiresIn: "10m" }
+    );
+
+    const refreshToken = jwt.sign(
+      {
+        _id: foundUser._id,
+      },
+      process.env.REFRESH_TOKEN_SECRET,
+      { expiresIn: "1d" }
+    );
+
+    foundUser.refreshToken = refreshToken;
+    const result = await foundUser.save();
+
+    res.cookie("token", refreshToken, {
+      httpOnly: true,
+      sameSite: "None",
+      maxAge: 24 * 60 * 60 * 1000, // 86400초(하루)
+      secure: true,
+    }); //secure: true,
+
+    res.status(201).json({ accessToken });
+  } else {
+    res.sendStatus(500);
+  }
+};
+
+module.exports = { handleLogin };
