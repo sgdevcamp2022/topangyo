24a4a490e0441bde768a22cdf6c28c23247ce7e9
우명규 <myeong@umyeong-gyuui-MacBookPro.local>
2023-01-28 20:53:32 +0900

    test : test signup server & client

--- server_auth/controllers/regiesterController.js
+++ server_auth/controllers/regiesterController.js
@@ -2,6 +2,8 @@ const User = require("../models/User");
 const bcrypt = require("bcrypt");
 
 const handleNewUserRegist = async (req, res) => {
+  console.log(req.body);
+  console.log(typeof req.body);
   try {
     const { id, password, name, nickname, email, phoneNumber, birth, gender } =
       req.body;
@@ -45,6 +47,7 @@ const handleNewUserRegist = async (req, res) => {
 
     res.sendStatus(201);
   } catch (error) {
+    console.log(error);
     res.sendStatus(500);
   }
 };
