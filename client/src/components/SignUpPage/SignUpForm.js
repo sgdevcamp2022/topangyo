705f9842bfb15ab5d840a40cff8c2c403366289f
01star01ek <01star01ek@gmail.com>
2023-01-28 16:20:39 +0900

    feat:addSignUP

--- client/src/components/SignUpPage/SignUpForm.js
+++ client/src/components/SignUpPage/SignUpForm.js
@@ -1,3 +1,5 @@
+import '../../styles/SignUpPage.scss';
+
 const SignUpForm = () => {
     
     const handleSubmit = (e) => {
@@ -7,17 +9,19 @@ const SignUpForm = () => {
 
 	return(
         <>
-            <form className="signUp-box" onSubmit={handleSubmit}>
+            <form className="signup-box" onSubmit={handleSubmit}>
                 <input className="input-box" name="id" type="text" required minLength={4} maxLength={30} placeholder = "ID"/>
                 <input className="input-box" name="password" type="password" required minLength={4} maxLength={30} placeholder = "PASSWORD"/>
                 <input className="input-box" name="name" type="text" required minLength={4} maxLength={10} placeholder = "NAME"/>
                 <input className="input-box" name="nickname" type="text" required minLength={4} maxLength={10} placeholder = "NICKNAME"/>
                 <input className="input-box" name="birth" type="date"/>
-                <input className="input-box" name="email" type="email"  placeholder="example@gmail.com" required/>
+                <input className="input-box" name="email" type="email"  placeholder="EMAIL (example@gmail.com)" required minLength={4} maxLength={30} required/>
                 <input className="input-box" name ="phone" type = "number" placeholder="phone Number"/>
-                <input type='radio' name='gender' value='1' />남
+                <form className='gender-box'>
+                <input type='radio' name='gender' value='1' checked/>남
                 <input type='radio' name='gender' value='2' />여
-                <button type='submit'>Sign Up</button>
+                </form>
+                <button type='submit' name='button'>Sign Up</button>
             </form>
             <p className = "signUpText">Already have an account? <span className = "signUpWord">Sign In</span></p>
         </>
