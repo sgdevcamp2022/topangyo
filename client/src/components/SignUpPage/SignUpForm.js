b01748a23c4e152d73d510545320c671250afd93
우명규 <myeong@umyeong-gyuui-MacBookPro.local>
2023-01-28 19:41:52 +0900

    feat : Add signup function

--- client/src/components/SignUpPage/SignUpForm.js
+++ client/src/components/SignUpPage/SignUpForm.js
@@ -1,26 +1,126 @@
+import { useState } from 'react';
 import '../../styles/SignUpPage.scss';
 
 const SignUpForm = () => {
-    
+
+    const [registerUser, setRegisterUser] = useState({
+        id : "",
+        password : "",
+        name : "",
+        nickname : "",
+        birth : "",
+        email : "",
+        phoneNumber : "",
+        gender : 1,
+    })
+
+    const handleChange = (e) => {
+        setRegisterUser({
+            ...registerUser,
+            [e.target.name] : e.target.value,
+        })
+    }   
+
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log('회원가입 제출 완료!');
+
+        const variables = {
+            id : registerUser.id,
+            password : registerUser.password,
+            name : registerUser.name,
+            nickname : registerUser.nickname,
+            birth : registerUser.birth,
+            email : registerUser.email,
+            phoneNumber : registerUser.phoneNumber,
+            gender : parseInt(registerUser.gender)
+        }
+        
     }
 
 	return(
         <>
             <form className="signup-box" onSubmit={handleSubmit}>
-                <input className="input-box" name="id" type="text" required minLength={4} maxLength={30} placeholder = "ID"/>
-                <input className="input-box" name="password" type="password" required minLength={4} maxLength={30} placeholder = "PASSWORD"/>
-                <input className="input-box" name="name" type="text" required minLength={4} maxLength={10} placeholder = "NAME"/>
-                <input className="input-box" name="nickname" type="text" required minLength={4} maxLength={10} placeholder = "NICKNAME"/>
-                <input className="input-box" name="birth" type="date"/>
-                <input className="input-box" name="email" type="email"  placeholder="EMAIL (example@gmail.com)" required minLength={4} maxLength={30} required/>
-                <input className="input-box" name ="phone" type = "number" placeholder="phone Number"/>
-                <form className='gender-box'>
-                <input type='radio' name='gender' value='1' checked/>남
-                <input type='radio' name='gender' value='2' />여
-                </form>
+                <input 
+                    onChange={handleChange} 
+                    className="input-box" 
+                    name="id" 
+                    type="text" 
+                    minLength={4} 
+                    maxLength={20} 
+                    placeholder = "ID"
+                />
+                <input 
+                    onChange={handleChange} 
+                    className="input-box" 
+                    name="password" 
+                    type="password" 
+                    minLength={4} 
+                    maxLength={20} 
+                    placeholder = "PASSWORD"
+                />
+                <input 
+                    onChange={handleChange}
+                    className="input-box" 
+                    name="name" 
+                    type="text" 
+                    minLength={1} 
+                    maxLength={10} 
+                    placeholder = "NAME"
+                />
+                <input 
+                    onChange={handleChange} 
+                    className="input-box" 
+                    name="nickname" 
+                    type="text" 
+                    minLength={2} 
+                    maxLength={10} 
+                    placeholder = "NICKNAME"
+                />
+                <input 
+                    onChange={handleChange} 
+                    className="input-box" 
+                    name="birth" 
+                    type="date"
+                />
+                <input 
+                    onChange={handleChange} 
+                    className="input-box" 
+                    name="email" 
+                    type="email" 
+                    minLength={4} 
+                    maxLength={30}
+                    placeholder="EMAIL (example@gmail.com)" 
+                />
+                <input 
+                    onChange={handleChange} 
+                    className="input-box" 
+                    name ="phoneNumber" 
+                    type = "number" 
+                    placeholder="phone Number (01012345678)"
+                />
+                <>
+                    <label htmlFor='men'>
+                        <input 
+                            onChange={handleChange} 
+                            type='radio' 
+                            id='men' 
+                            name='gender' 
+                            value="1"
+                            defaultChecked
+                        />
+                        남자
+                    </label>
+                    <label htmlFor='women'>
+                        <input 
+                            onChange={handleChange} 
+                            type='radio' 
+                            id='women'
+                            name='gender' 
+                            value="2"
+                        />
+                        여자
+                    </label>
+                </>
                 <button type='submit'>Sign Up</button>
             </form>
             <p className = "signInText">Already have an account? <span className = "signInWord">Sign In</span></p>
