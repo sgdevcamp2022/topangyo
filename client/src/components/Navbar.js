a2ff666b50e82c410050c2fbd5f25f97d5ed319d
우명규 <myeong@umyeong-gyuui-MacBookPro.local>
2023-01-29 22:21:44 +0900

    feat : Valid test signin & handle accesstoken

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,21 +1,39 @@
 import React from 'react'
 import '../styles/Navbar.scss';
+import { useSelector } from 'react-redux';
 
 const Navbar = () => {
+
+  const user = useSelector((state) => state.user)
+  console.log(user);
+
   return (
     <header>
         <a className='logo-box' href='#'>
             <img alt='logo' className='logo' src='images/logo/DSG_white_logo.png' />
             <h3>DSG</h3> 
         </a>
-        {/* 로그인 하기 전에 보여주는 Navbar */}
-        {/* <a className='signin-button' href='#'><button>Log in</button></a> */}
         
-        {/* 로그인 했을 때 보여주는 Navbar */}
-        <a className='user-box' href='#'>
-          <img alt='userImage' className='user-icon' src='images/user/user_image.png' />
-          <h4>Name</h4>
-        </a>
+        
+
+        {
+          !user.accessToken ?
+          (
+            //로그인 하기 전에 보여주는 Navbar
+            <a className='signin-button' href='#'><button>Log in</button></a>
+          )
+          :
+          (
+            //로그인 했을 때 보여주는 Navbar
+            <a className='user-box' href='#'>
+              <img alt='userImage' className='user-icon' src='images/user/user_image.png' />
+              <h4>{user.nickname}</h4>
+            </a>
+          )
+        }
+        
+        
+        
     </header>
   )
 }
--- client/src/components/SignInPage/SignInForm.js
+++ client/src/components/SignInPage/SignInForm.js
@@ -1,9 +1,15 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import axios from 'axios';
 import jwt_decode from 'jwt-decode';
+import { useSelector, useDispatch } from 'react-redux';
+import { changeGender, changeId, changeNickname, changeToken } from '../../store/store';
 import '../../styles/SignInPage.scss';
 
 const SignInForm = () => {
+
+    const dispatch = useDispatch();
+
+    const user = useSelector((state) => state.user);
 
     const [loginUser, setLoginUser] = useState({
         id : "",
@@ -24,13 +30,44 @@ const SignInForm = () => {
             id : loginUser.id,
             password : loginUser.password
         }
+        
+        
 
+        //Mock data용
+        // axios.get('http://localhost:3000/data/sample.json')
+        // .then((response) => {
+        //     const token = response.data.accessToken;
+        //     dispatch(changeToken(token))
+
+        //     const decode = jwt_decode(token);
+        //     dispatch(changeId(decode.userInfo.id));
+        //     dispatch(changeNickname(decode.userInfo.nickname));
+        //     dispatch(changeGender(decode.userInfo.gender));
+        // })
+        
+
+        //server용
         axios.post('http://localhost:3500/auth/login', variables)
         .then((response) => {
             const token = response.data.accessToken;
+            dispatch(changeToken(token))
+
             const decode = jwt_decode(token);
-            console.log(decode);
+            dispatch(changeId(decode.userInfo.id));
+            dispatch(changeNickname(decode.userInfo.nickname));
+            dispatch(changeGender(decode.userInfo.gender));
         })
+        .catch((error) => {
+            console.log(error.response.status);
+            switch(error.response.status) {
+                case 400:
+                    alert('입력한 input들의 type이 정확하지 않습니다');
+                    break;
+                case 500:
+                    alert('아이디 또는 비밀번호가 일치하지 않습니다');
+                    break;
+            }
+        }) 
     }
 
     return (
@@ -40,6 +77,10 @@ const SignInForm = () => {
                 <input onChange={handleChange} className="input-box" name="id" type="text" required minLength={4} maxLength={30} placeholder = "ID"/>
                 <input onChange={handleChange} className="input-box" name="password" type="password" required minLength={4} maxLength={30} placeholder = "PASSWORD"/>
                 <button type='submit'>Log in</button>
+                <div>{user.accessToken}</div>
+                <div>{user.nickname}</div>
+                <div>{user.id}</div>
+                <div>{user.gender}</div>
             </form>
             <p className = "signUpText">Don't have an account? <span className = "signUpWord">Sign Up</span></p>
             <p className = "forgotPassWordText">forgot Password?</p>
