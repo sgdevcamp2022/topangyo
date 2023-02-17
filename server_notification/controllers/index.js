899d9a1372308c2b865bfb400bedea48e7fcdb1d
sayoP <swj7852@gmail.com>
2023-02-17 11:39:26 +0900

    add notification_server

---
+++ server_notification/controllers/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const { async } = require('q');
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+    res.json({
+        status: 'ok',
+        message: 'Server is running'
+    });
+});
+
+module.exports = router;
