7a922253110f572719aae1260da6e5ed2dd7c90e
smaivnn <janus321@naver.com>
2023-02-21 23:19:04 +0900

    fix : many things

--- client/src/components/HomePage/MatchingDetail.js
+++ client/src/components/HomePage/MatchingDetail.js
@@ -8,15 +8,17 @@ import { useDispatch, useSelector } from "react-redux";
 import { closeModal } from "../../store/slice/modalslice";
 import axios from "axios";
 import { setPlaceSearch } from "../../store/slice/placeslice";
-import './../../styles/MatchingDetail.scss';
+import "./../../styles/MatchingDetail.scss";
 const CONNECT_URL_SOCKET = "http://localhost:4000/chat"; // 소켓 주소
 const socket = io.connect(CONNECT_URL_SOCKET); // 채팅 소켓 연결
 
 const MatchingDetail = () => {
   const dispatch = useDispatch();
   const currentPost = useSelector((state) => state.posts.currentPost);
-  const matchedMembersCount = useSelector((state) => state.matching.matchedMembersCount)
-  const modal = useSelector((state) => state.modal)
+  const matchedMembersCount = useSelector(
+    (state) => state.matching.matchedMembersCount
+  );
+  const modal = useSelector((state) => state.modal);
 
   const myStorage = localStorage;
   const getMatchingPostPK = JSON.parse(myStorage.getItem("matchingPostPK"));
@@ -30,7 +32,7 @@ const MatchingDetail = () => {
   const handleCloseModal = () => {
     dispatch(setPlaceSearch(false));
     dispatch(closeModal());
-    if(modal.place) {
+    if (modal.place) {
       window.location.reload();
     }
   };
@@ -83,24 +85,29 @@ const MatchingDetail = () => {
   const handleLeaveRoom = async () => {
     try {
       getMatchingPostPK.map((data, idx) => {
-        if(data === currentPost.postPK) {
-          getMatchingPostPK.splice(idx,1);
-          myStorage.setItem('matchingPostPK', JSON.stringify(getMatchingPostPK))
+        if (data === currentPost.postPK) {
+          getMatchingPostPK.splice(idx, 1);
+          myStorage.setItem(
+            "matchingPostPK",
+            JSON.stringify(getMatchingPostPK)
+          );
         }
-      })
-    } catch(err) {
-      console.log(err)
+      });
+    } catch (err) {
+      console.log(err);
     }
     handleCloseModal();
   };
 
   const onConfirmMatching = async () => {
     try {
-      const result = await axios.put(`http://localhost:3700/post/matchingstatus/${currentPost.postPK}`)
-    }catch(err) {
-      console.log(err)
+      const result = await axios.put(
+        `http://localhost:3700/post/matchingstatus/${currentPost.postPK}`
+      );
+    } catch (err) {
+      console.log(err);
     }
-  }
+  };
 
   // 이름 적절히 바꿀 것
   // 현재 상태에 따라 filtering하여서 밑에 버튼을 바꾼다.
@@ -110,52 +117,85 @@ const MatchingDetail = () => {
       applyUser?.includes(id) === false &&
       matchedMembers?.includes(id) === false
     )
-      return <button onClick={sendApplyment} className='application'>신청</button>;
+      return (
+        <button onClick={sendApplyment} className="application">
+          신청
+        </button>
+      );
     if (currentPost.author_id !== id && applyUser?.includes(id) === true)
-      return <button onClick={cancleApplyment} className='non-application'>신청취소</button>;
+      return (
+        <button onClick={cancleApplyment} className="non-application">
+          신청취소
+        </button>
+      );
     if (currentPost.author_id !== id && matchedMembers?.includes(id) === true)
-      return <button onClick={() => cancleMatching(id)} className='non-application'>매칭취소</button>;
-    if (currentPost.author_id === id) 
-      return <button onClick={onConfirmMatching} className='application'>확정</button>;
-    
+      return (
+        <button onClick={() => cancleMatching(id)} className="non-application">
+          매칭취소
+        </button>
+      );
+    if (currentPost.author_id === id)
+      return (
+        <button onClick={onConfirmMatching} className="application">
+          확정
+        </button>
+      );
   };
 
   return (
     <div className="chatpage">
-       <div className='chatpage-container'>
-          <div className="chat-main">
-            <div className='chat-first-text'>
-              <h2 className='chat-title'>{currentPost.title}</h2>
-              <div>모집글 상태</div>
-              <button style={{ padding : 0, backgroundColor : 'white' }} onClick={handleCloseModal}><img width="30px" className = "x-img" src = 'images/close.png' ></img></button>
-            </div>
-            <div className='chat-second-text'>
-                <div>매칭인원 : {matchedMembersCount.length} / 모집인원 : {currentPost.memberLimit}</div>
+      <div className="chatpage-container">
+        <div className="chat-main">
+          <div className="chat-first-text">
+            <h2 className="chat-title">{currentPost.title}</h2>
+            <button
+              style={{ padding: 0, backgroundColor: "white" }}
+              onClick={handleCloseModal}
+            >
+              <img width="30px" className="x-img" src="images/close.png"></img>
+            </button>
+          </div>
+          <div className="chat-second-text">
+            <div>
+              매칭인원 : {matchedMembersCount.length} / 모집인원 :{" "}
+              {currentPost.memberLimit}
             </div>
           </div>
-          <div className='chating'>
-              <MatchingChat socket={socket} room={room}/>
-              <div id="chat-column-line"></div>
-              <div className='chat-inform'>
-              <div className='inform'>
-                  <div className="inform-person">
-                    <MatchingUser
-                      socket={socket}
-                      room={room}
-                      id={id}
-                      currentPost={currentPost}
-                      applyUser={applyUser}
-                      setApplyUser={setApplyUser}
-                      matchedMembers={matchedMembers}
-                      setMatchedMembers={setMatchedMembers}
-                      cancleMatching={cancleMatching}
-                    /></div>
-                  <div className="inform-place"><MatchingPlace socket={socket} room={room} id={id}/></div>
-                  <div className="inform-time"><MatchingTime socket={socket} room={room} id={id}/></div>
+        </div>
+        <div className="chating">
+          <MatchingChat socket={socket} room={room} />
+          <div id="chat-column-line"></div>
+
+          <div className="chat-inform">
+            <div className="inform">
+              <div className="inform-person">
+                <MatchingUser
+                  socket={socket}
+                  room={room}
+                  id={id}
+                  currentPost={currentPost}
+                  applyUser={applyUser}
+                  setApplyUser={setApplyUser}
+                  matchedMembers={matchedMembers}
+                  setMatchedMembers={setMatchedMembers}
+                  cancleMatching={cancleMatching}
+                />
               </div>
-              <div className="inform-button">{btn()}<button className='out' onClick={handleLeaveRoom}>나가기</button></div>
+              <div className="inform-place">
+                <MatchingPlace socket={socket} room={room} id={id} />
+              </div>
+              <div className="inform-time">
+                <MatchingTime socket={socket} room={room} id={id} />
+              </div>
+            </div>
+            <div className="inform-button">
+              {btn()}
+              <button className="out" onClick={handleLeaveRoom}>
+                나가기
+              </button>
             </div>
           </div>
+        </div>
       </div>
     </div>
   );
